269a1c7cb0101f7fbd9ea921eccfcf3de95f0270
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2023-07-22 14:26:48 +0900

    HttpRequestを切り出した

---
+++ src/main/java/HttpRequest.java
@@ -0,0 +1,66 @@
+package main.java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static main.java.Constant.CRLF;
+
+public class HttpRequest {
+    private RequestLine requestLine;
+    private Map<String, String> headerField = new HashMap<>();
+
+    public HttpRequest(InputStream in) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+        this.requestLine = new RequestLine(reader.readLine());
+        putRequestHeaderField(reader);
+    }
+
+    private void putRequestHeaderField(BufferedReader reader) throws IOException {
+        String line = reader.readLine();
+        while(!line.isEmpty()) {
+            List<String> field = Arrays.stream(line.split(":")).map(String::trim).toList();
+            headerField.put(field.get(0), field.get(1));
+            line = reader.readLine();
+        }
+    }
+
+    public void stdOutputMessage() {
+        stdOutputRequestLine();
+        stdOutputRequestHeaderField();
+    }
+
+    private void stdOutputRequestLine() {
+        System.out.println(this.requestLine);
+    }
+
+    private void stdOutputRequestHeaderField() {
+        headerField.entrySet()
+                .forEach(field -> System.out.printf("%s: %s%s", field.getKey(), field.getValue(), CRLF));
+    }
+}
+
+class RequestLine {
+    private String requestLine;
+    private String httpMethod;
+    private String path;  // Pathクラスみたいなやつあった気がするが一旦ストリング
+    private String protocolVersion;
+
+    public RequestLine(String requestLine) {
+        this.requestLine = requestLine;
+        String[] requestLineElements = requestLine.split(" ");
+        this.httpMethod = requestLineElements[0];
+        this.path = requestLineElements[1];
+        this.protocolVersion = requestLineElements[2];
+    }
+
+    @Override
+    public String toString() {
+        return this.requestLine;
+    }
+}

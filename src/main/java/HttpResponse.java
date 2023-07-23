77e007a135a7df43890f3fd25aa9afad2ba3a0dd
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2023-07-23 23:37:30 +0900

    respond proper Content-Type based on requested file's extension

--- src/main/java/HttpResponse.java
+++ src/main/java/HttpResponse.java
@@ -7,6 +7,7 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.stream.Stream;
 
 import static main.java.Constant.*;
 
@@ -15,13 +16,15 @@ public class HttpResponse {
     private final HttpRequest request;
     private final StatusLine statusLine;
     private final OutputStream out;
-    private Map<String, String> headerField = new HashMap<>();
+    private Map<String, String> headerFieldMap = new HashMap<>();
     private String body;
+    
 
     public HttpResponse(OutputStream out, HttpRequest request, Status status) {
         this.request = request;
         this.out = out;
         statusLine = new StatusLine(status);
+        headerFieldMap.put("Content-Type", ContentType.getMIMEType(request.getRequestLine().getPath()));
     }
 
     public void response() throws IOException {
@@ -35,17 +38,24 @@ public class HttpResponse {
        this.out.write(String.format("%s %s%s", this.statusLine.getProtocolVersion(), this.statusLine.getStatus(), CRLF).getBytes());
     }
 
-    private void writeHeaderFields() throws IOException {
-        // TODO: Content-Typeをどうにかいい感じにする(mapにいれたヘッダーフィールドを返す)
-        this.out.write("Content-type: text/html; charset=UTF-8\r\n".getBytes());
+    private void writeHeaderFields() {
+        headerFieldMap.forEach((key, value) -> {
+            try {
+                this.out.write(String.format("%s: %s%s", key, value, CRLF).getBytes());
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
     }
 
     private void writeBody() throws IOException {
         HttpRequest.RequestLine requestLine = this.request.getRequestLine();
+        Path path = requestLine.getPath();
         // GETかつtext/html
-        if(Objects.equals(requestLine.getHttpMethod(), HttpMethod.GET.name()) && isContentTypeHtml()) {
+        if(Objects.equals(requestLine.getHttpMethod(), HttpMethod.GET.name()) 
+                && Objects.equals(ContentType.getMIMEType(path), "text/html")) {
             if(this.statusLine.getStatus() == Status.OK) {
-                this.out.write(Files.readString(requestLine.getPath()).getBytes());
+                this.out.write(Files.readString(path).getBytes());
             } else if (this.statusLine.getStatus() == Status.NOT_FOUND) {
                 this.out.write(Files.readString(NOT_FOUND_PAGE_PATH).getBytes());
             }
@@ -59,9 +69,6 @@ public class HttpResponse {
 
 
 
-    // memo
-    // - make enum of content-type
-    
     class StatusLine {
         private final String protocolVersion = "HTTP/1.1";
         private final Status status;
